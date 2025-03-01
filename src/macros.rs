972a435430a9a57f64e5302f673d5439bcc75083
storycraft <storycraft@pancake.sh>
2025-03-01 21:32:05 +0900

    feat: add ForLt macro

--- src/macros.rs
+++ src/macros.rs
@@ -2,16 +2,29 @@
 macro_rules! LinkedList {
     (for<$lt:lifetime> $arg:ty) => {
         $crate::LinkedList<
-            $crate::hkt::Wrapper<
-                dyn for<$lt> $crate::hkt::Hkt<
-                    $lt,
-                    T = $arg
-                >
-            >
+            $crate::ForLt!(
+                for<$lt> $arg
+            )
         >
     };
 
     ($arg:ty) => {
         $crate::LinkedList!(for<'a> $arg)
+    };
+}
+
+#[macro_export]
+macro_rules! ForLt {
+    (for<$lt:lifetime> $arg:ty) => {
+        $crate::hkt::Wrapper<
+            dyn for<$lt> $crate::hkt::Hkt<
+                $lt,
+                T = $arg
+            >
+        >
+    };
+
+    ($arg:ty) => {
+        $crate::ForLt!(for<'a> $arg)
     };
 }
